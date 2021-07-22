9b4c058429edeb280c0cb635bdaf9c7a7296a92b
netbull <alexander.dimitrov@slr-holiday-service.bg>
2021-07-22 13:07:13 +0300

    fix: minor adjustments

--- src/Twig/EmailWebpackEncoreExtension.php
+++ src/Twig/EmailWebpackEncoreExtension.php
@@ -13,72 +13,74 @@ use Twig\TwigFunction;
  */
 class EmailWebpackEncoreExtension extends EmailExtension implements ServiceSubscriberInterface
 {
-	/**
-	 * @var EntrypointLookupInterface
-	 */
-	private EntrypointLookupInterface $entrypointLookup;
+    /**
+     * @var EntrypointLookupInterface
+     */
+    private EntrypointLookupInterface $entrypointLookup;
 
-	/**
-	 * @var string
-	 */
-	private string $publicDir;
+    /**
+     * @var string
+     */
+    private string $publicDir;
 
     /**
      * EmailExtensionWebpackEncore constructor.
      * @param EntrypointLookupInterface $entrypointLookup
      * @param ParameterBagInterface $parameterBag
      */
-	public function __construct(ParameterBagInterface $parameterBag, EntrypointLookupInterface $entrypointLookup)
-	{
-	    parent::__construct($parameterBag);
+    public function __construct(ParameterBagInterface $parameterBag, EntrypointLookupInterface $entrypointLookup)
+    {
+        parent::__construct($parameterBag);
 
-		$this->entrypointLookup = $entrypointLookup;
-		$this->publicDir = $parameterBag->get('kernel.project_dir').'/public';
-	}
+        $this->entrypointLookup = $entrypointLookup;
+        $this->publicDir = $parameterBag->get('kernel.project_dir').'/public';
+    }
 
-	/**
-	 * @return TwigFunction[]
-	 */
-	public function getFunctions(): array
-	{
-		return [
-			new TwigFunction('css_source', [$this, 'getCssSource']),
-		];
-	}
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('css_source', [$this, 'getCssSource']),
+        ];
+    }
 
     /**
      * @param string|null $file Entry name
      * @return string
      */
-	public function getCssSource(string $file = null): string
-	{
-		$files = $this->entrypointLookup->getCssFiles($file);
-		$context = null;
+    public function getCssSource(string $file = null): string
+    {
+        $context = null;
 
-		$source = parent::getCssSource($file);
-		foreach ($files as $f) {
-			if (0 === strpos($f, '/')) {
-                $f = $this->publicDir.$f;
-			}
+        $source = parent::getCssSource($file);
+        if ($file) {
+            $files = $this->entrypointLookup->getCssFiles($file);
+            foreach ($files as $f) {
+                if (0 === strpos($f, '/')) {
+                    $f = $this->publicDir.$f;
+                }
 
-			if (false === strpos($f, 'http') && !file_exists($f)) {
-				continue;
-			}
+                if (false === strpos($f, 'http') && !file_exists($f)) {
+                    continue;
+                }
 
-			$source .= file_get_contents($f, false, $context);
-		}
+                $source .= file_get_contents($f, false, $context);
+            }
 
-		$this->entrypointLookup->reset();
-		return $source;
-	}
+            $this->entrypointLookup->reset();
+        }
+        return $source;
+    }
 
-	/**
-	 * @return string[]
-	 */
-	public static function getSubscribedServices(): array
-	{
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedServices(): array
+    {
         return [
             EntrypointLookupInterface::class,
         ];
-	}
+    }
 }
